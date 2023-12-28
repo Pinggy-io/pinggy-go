41046fab8b4b75239906c7ffc20d4695b14a720a
Abhijit Mondal <abhijit.manpur@gmail.com>
2023-12-28 21:13:35 +0530

    Adding Examples

---
+++ pinggy/examples/udptunnel/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/Pinggy-io/pinggy-go/pinggy/udptunnel"
+)
+
+func main() {
+	if len(os.Args) <= 2 {
+		fmt.Println(len(os.Args), os.Args)
+		os.Exit(3)
+	}
+
+	tcpPort, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	forwarder, err := udptunnel.NewTunnelMangerListen(tcpPort, os.Args[2])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	forwarder.StartForwarding()
+}
